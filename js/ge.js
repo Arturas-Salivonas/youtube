158e4b937ca3f2a15b57f1429a15c530a1fd4f1d
Arturas Salivonas <ecobra@gmail.com>
2017-01-19 23:06:41 +0000

    implementation youtube api

--- js/ge.js
+++ js/ge.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 								$.each(data.items, function(i, item){
 									console.log(item);
 									videoId = item.snippet.resourceId.videoId;
-									output = '<div class="col-lg-6 col-md-6 wow zoomIn" data-wow-delay="0.2s" style="visibility: visible; -webkit-animation-delay: 0.2s; -moz-animation-delay: 0.2s; animation-delay: 0.2s;" ><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></div>';
+									output = '<div class="col-lg-6 col-md-6 youtubelist" ><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></div>';
 
 									$('#results').append(output);
 								})

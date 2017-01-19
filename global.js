5a527a3cd0b65eea4822d92fb7082e3d49b45272
Arturas Salivonas <ecobra@gmail.com>
2017-01-19 21:48:36 +0000

    implementation

--- global.js
+++ global.js
@@ -1,13 +1,35 @@
 var channelName = 'GrowthEngineer';
-$(document).ready(function()){
+$(document).ready(function(){
 	$.get(
-		"GET https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=GrowthEngineer&key=AIzaSyAYqrh_EYmrdVwK2Zhm_QuzHemzVJmGWXw",{
+		"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=GrowthEngineer&key=AIzaSyAYqrh_EYmrdVwK2Zhm_QuzHemzVJmGWXw",{
 			part: 'contentDetails',
 			forUsername: channelName,
 			key: 'AIzaSyAYqrh_EYmrdVwK2Zhm_QuzHemzVJmGWXw'},
 			function(data){
-					$.each(data.items, function{i, item}{
+					$.each(data.items, function(i, item){
 						console.log(item);
+						pid = item.contentDetails.relatedPlaylists.uploads;
+						getVids(pid);
 					})
-			};
-)};
+				}
+			);
+			function getVids(pid){
+				$.get(
+					"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=GrowthEngineer&key=AIzaSyAYqrh_EYmrdVwK2Zhm_QuzHemzVJmGWXw",{
+						part: 'snippet',
+						maxResults: 10,
+						playlistId: pid
+						key: 'AIzaSyAYqrh_EYmrdVwK2Zhm_QuzHemzVJmGWXw'},
+						function(data){
+							var output;
+								$.each(data.items, function(i, item){
+									console.log(item);
+									videoTitle = item.snippet.title;
+									output = '<li>+videoTitle+</li>';
+
+									$('#results').append(output);
+								})
+							}
+						);
+			}
+});
